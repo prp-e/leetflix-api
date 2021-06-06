a275f888e01855d556babf67ec2c989c44035612
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-06-07 00:04:17 +0430

    Flask CORS added to the mix

--- api.py
+++ api.py
@@ -1,14 +1,16 @@
 from flask import Flask, json, jsonify, request
+from flask_cors import CORS
 import search
 
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.post('/')
 def main_endpoint():
     input_data = request.get_json()
     keyword = input_data['keyword']
     data = search.gather_data(keyword)
-    
+
     return jsonify(data)
 
 if __name__ == "__main__":
